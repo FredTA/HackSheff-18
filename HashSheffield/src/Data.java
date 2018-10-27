069a04477b77062e2fd8de7d8c4eb4be815dad5e
rthandi <robbie.thandi@gmail.com>
2018-10-27 15:13:03 +0100

    Changed the inputs for Data

--- HashSheffield/src/Data.java
+++ HashSheffield/src/Data.java
@@ -2,23 +2,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Data {
-    public static void createNewPass(String hashedPassword, String nameOfService, String date) throws IOException {
+    public static void createNewPass(String[] data) throws IOException {
         try{
             String pathToFile = "passwords.txt";
             FileWriter writer = new FileWriter(pathToFile);
-            String[] data;
-            data = new String[3];
-            data[0] = hashedPassword;
-            data[1] = nameOfService;
-            data[2] = date;
             writer.write(data.toString());
         }
         catch (IOException e){
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        createNewPass("aasdbvaskhdbaksd", "askfjhdbasjhkbd", "sfkjasdbkj");
     }
 }
