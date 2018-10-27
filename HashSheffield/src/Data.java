bb422e7ce2b5e05f0717176da96eae49569456d7
FredTA <ftovey-ansell1@sheffield.ac.uk>
2018-10-27 18:37:09 +0100

    Data class now separates string correctly

--- HashSheffield/src/Data.java
+++ HashSheffield/src/Data.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Objects;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -37,33 +36,39 @@ public class Data {
 
     public String[][] convertList(ArrayList<String> inputList){
         String [][] outputList = new String[inputList.size()][3];
+
+        boolean colon1Found = false;
+        boolean colon2Found = false;
+        String subString = "";
+
         for (int i=0; i<inputList.size(); i++){
-            boolean flag1 = false;
-            boolean flag2 = false;
-            StringBuilder string1 = new StringBuilder();
-            StringBuilder string2 = new StringBuilder();
-            for (int j = 0; j < inputList.get(i).length(); j++) {
-                if (!flag1){
-                    if (Objects.equals(inputList.get(i).substring(j, j + 1), ";")){
-                        flag1 = true;
-                        outputList[i][0] = string1.toString();
-                    }
-                    else {
-                        string1.append(inputList.get(i).substring(j, j + 1));
-                    }
+            String inputString = inputList.get(i);
+            for (int elem = 0; elem < inputString.length(); elem++) {
+                if (inputString.charAt(elem) != ';')  {
+                    subString += inputString.charAt(elem);
                 }
-                else if (!flag2){
-                    if (Objects.equals(inputList.get(i).substring(j, j + 1), ";")){
-                        outputList[i][1] = string2.toString();
-                        outputList[i][2] = inputList.get(i).substring(j+1);
-                        break;
-                    }
-                    else {
-                        string2.append(inputList.get(i).substring(j, j + 1));
-                    }
+                else if (!colon1Found) {
+                    colon1Found = true;
+                    outputList[i][0] = subString;
+                    subString = "";
+                }
+                else if (!colon2Found){
+                    colon2Found = true;
+                    outputList[i][1] = subString;
+                    subString = "";
                 }
             }
+            colon1Found = colon2Found = false;
+            outputList[i][2] = subString;
+            subString = "";
         }
+ /*
+        for (int i = 0; i < outputList.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(outputList[i][j] + " ");
+            }
+            System.out.println("");
+        } */
         return outputList;
     }
 
