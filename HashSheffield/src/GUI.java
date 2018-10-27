7396192cb0ed6bbd86ea6a1b31819a08390050ab
FredTA <ftovey-ansell1@sheffield.ac.uk>
2018-10-27 13:05:17 +0100

    Replaced project files, it was borked

--- HashSheffield/src/GUI.java
+++ HashSheffield/src/GUI.java
@@ -1,8 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class GUI {
+public class GUI extends JFrame {
 
-    public GUI() {
+    private Container contentPane;
 
+    //The size of screen of the device the program is running on
+    private Dimension screenSize;
+
+    public GUI()  {
+        super("HashSheffield");
+
+        contentPane = getContentPane();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        //Get the size of the screen from the awt toolkit
+        Toolkit windowToolkit = Toolkit.getDefaultToolkit();
+        screenSize = windowToolkit.getScreenSize();
     }
 }
+
