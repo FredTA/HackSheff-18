546b2db2b1768db1ee6c9c06cf60c3f3a48b84fb
rthandi <robbie.thandi@gmail.com>
2018-10-27 17:11:16 +0100

    Added conversion method to data.

--- HashSheffield/src/Data.java
+++ HashSheffield/src/Data.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class Data {
 
-    File pathToFile = new File("passwords.txt");
+    private File pathToFile = new File("passwords.txt");
 
     public void createNewPass(String data) throws IOException {
         try{
@@ -33,4 +33,24 @@ public class Data {
             return list;
         }
     }
+
+    public String[][] convertList(ArrayList<String> inputList){
+        String [][] outputList = new String[inputList.size()][3];
+        for (int i=0; i<inputList.size(); i++){
+            for (int j=0; j<3; j++){
+                int locationOfColon = inputList.get(i).indexOf(";");
+                outputList[i][0] = inputList.get(i).substring(0, locationOfColon - 1);
+                int locationOfColon2 = inputList.get(i).substring(locationOfColon).indexOf(";");
+                outputList[i][1] = inputList.get(i).substring(locationOfColon + 1, locationOfColon2 - 1);
+                outputList[i][2] = inputList.get(i).substring(locationOfColon2 + 1);
+            }
+        }
+        return outputList;
+    }
+
+//    public static void main(String[] args) {
+//        ArrayList<String> list = new ArrayList<>();
+//        list.add("hello;hello1;hello2");
+//        System.out.println(convertList(list));
+//    }
 }
