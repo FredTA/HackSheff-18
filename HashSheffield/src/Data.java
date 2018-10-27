d22f9a6f2b6ab6667c870b10ccd87d92f2205d3a
rthandi <robbie.thandi@gmail.com>
2018-10-27 15:56:27 +0100

    Added read in for Data.

--- HashSheffield/src/Data.java
+++ HashSheffield/src/Data.java
@@ -1,17 +1,29 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Data {
-    public static void createNewPass(String data) throws IOException {
+
+    String pathToFile = "passwords.txt";
+
+    public void createNewPass(String data) throws IOException {
         try{
-            String pathToFile = "passwords.txt";
+//            String pathToFile = "passwords.txt";
             FileWriter writer = new FileWriter(pathToFile);
-            //System.out.println(data.toString());
             writer.write(data);
             writer.close();
         }
         catch (IOException e){
             e.printStackTrace();
         }
+    }
+    public ArrayList<String> readData(){
+        Scanner scanner = new Scanner(pathToFile);
+        ArrayList<String> list = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            list.add(scanner.next());
+        }
+        return list;
     }
 }
