d96df80d062fd73ab6f6161ed07b93b1a4fb5998
CianMoriarty <cmoriarty1@sheffield.ac.uk>
2018-10-27 18:57:19 +0100

    Added some memes to main Also Added the dooter

--- HashSheffield/src/Main.java
+++ HashSheffield/src/Main.java
@@ -2,8 +2,13 @@ import java.io.FileNotFoundException;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
         GUI display = new GUI();
+		
+		HttpURLConnectionExample http = new HttpURLConnectionExample();
+
+		System.out.println("Testing 1 - Send Http GET request");
+		http.sendGet();
     }
 }
