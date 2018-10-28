87c1480a67749e117c93f4f29be0ab693ea6b019
CianMoriarty <cmoriarty1@sheffield.ac.uk>
2018-10-28 10:29:29 +0000

    12345

--- HashSheffield/src/Main.java
+++ HashSheffield/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
-       // GUI display = new GUI();
+        GUI display = new GUI();
 		
 		HttpURLConnectionExample http = new HttpURLConnectionExample();
 		System.out.println("Testing 1 - Send Http GET request");
