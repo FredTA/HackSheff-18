d02bb311b286a79d30f0e7fb145c8cb9ed828589
rthandi <robbie.thandi@gmail.com>
2018-10-28 02:27:58 +0000

    Fixed some more of Cian's stuff but it won't work yet cause he needs to add things.

--- HashSheffield/src/Main.java
+++ HashSheffield/src/Main.java
@@ -6,9 +6,9 @@ public class Main {
         System.out.println("Hello World!");
         GUI display = new GUI();
 		
-//		HttpURLConnectionExample http = new HttpURLConnectionExample();
+		HttpURLConnectionExample http = new HttpURLConnectionExample();
 		System.out.println("Testing 1 - Send Http GET request");
-//		http.printStr(http.sendGet());
+		http.printStr(http.sendGet());
 
 		
 		
