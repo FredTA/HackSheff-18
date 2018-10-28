0e05b22f355cf09a2ae9e061518cfb104bdeaf2b
CianMoriarty <cmoriarty1@sheffield.ac.uk>
2018-10-28 10:26:17 +0000

    1234

--- HashSheffield/src/HttpURLConnectionExample.java
+++ HashSheffield/src/HttpURLConnectionExample.java
@@ -3,13 +3,15 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class HttpURLConnectionExample {
 
 	private final String USER_AGENT = "Mozilla/5.0";
+	int h = 0;
 
 	// HTTP GET request
-	public StringBuffer sendGet() throws Exception {
+	public String sendGet() throws Exception {
 
 		String url = "https://haveibeenpwned.com/api/v2/breaches";
 		
@@ -23,8 +25,7 @@ public class HttpURLConnectionExample {
 		con.setRequestProperty("User-Agent", USER_AGENT);
 
 		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
+
 
 		BufferedReader in = new BufferedReader(
 			new InputStreamReader(con.getInputStream()));
@@ -34,24 +35,27 @@ public class HttpURLConnectionExample {
 		while ((inputLine = in.readLine()) != null) {
 			response.append(inputLine);
 		}
-		return response;
+		String strNew = response.toString();
+		//String strNew = str.substring(1, str.length()-1);
+		return strNew;
 		
 	}
 		
-	void printStr(StringBuffer rsp) {
+	void printStr(String strNew) {
 		
-		String str = rsp.toString();
 		
-		String strNew1 = str.substring(1, str.length()-1);
-		JSONObject obj2 = new JSONObject(strNew1);
-		
+		JSONArray arr = new JSONArray(strNew);
+		JSONObject obj2 = arr.getJSONObject(h);
 		String n = obj2.getString("Domain");
 		String a = obj2.getString("BreachDate");
+		h += 1;
 		
-		System.out.println(n + "/n " + a); 
-
-		strNew1 = strNew1.replaceFirst("Domain","");
+		System.out.println(n + " " + a); 
 		
 		
+		
+		if (h<arr.length()) {
+			printStr(strNew);
+		}
 	}
 }
--- HashSheffield/src/Main.java
+++ HashSheffield/src/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
-        GUI display = new GUI();
+       // GUI display = new GUI();
 		
 		HttpURLConnectionExample http = new HttpURLConnectionExample();
 		System.out.println("Testing 1 - Send Http GET request");
+		
 		http.printStr(http.sendGet());
 
-		
-		
+
     }
 }
